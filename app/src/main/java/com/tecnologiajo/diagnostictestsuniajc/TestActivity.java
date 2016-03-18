4aaa9e1c3ac6363bc0e9e5a132bf02b2ae5ca9cf
jorge <alexortizcortes@gmail.com>
2016-03-18 14:30:38 -0500

    asignacion de preguntas al los Textview del diseño

--- app/src/main/java/com/tecnologiajo/diagnostictestsuniajc/TestActivity.java
+++ app/src/main/java/com/tecnologiajo/diagnostictestsuniajc/TestActivity.java
@@ -1,5 +1,8 @@
 package com.tecnologiajo.diagnostictestsuniajc;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
@@ -18,15 +21,29 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.ViewAnimator;
 
+import com.shephertz.app42.paas.sdk.android.App42Exception;
+import com.shephertz.app42.paas.sdk.android.storage.Storage;
 import com.tecnologiajo.diagnostictestsuniajc.R;
 
-public class TestActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class TestActivity extends AppCompatActivity implements AsyncApp42ServiceApi.App42StorageServiceListener{
 
     public boolean seleccion,finalizar;
     public ViewAnimator viewAnimator;
-    public TextView timeTest;
+    public TextView timeTest,textquestion,Q1,Q2,Q3,Q4;
     public RelativeLayout relativeLayout;
     Animation slide_in_left, slide_out_right;
+    /** The async service. */
+    private AsyncApp42ServiceApi asyncService;
+    /** The progress dialog. */
+    private ProgressDialog progressDialog;
+    /** The doc id. */
+    private String docId = "56e0c3cae4b0e89150c63cba";
+    private String docDocument="";
+    JSONArray jsonArray;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +59,14 @@ public class TestActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });*/
+
+        textquestion =(TextView) findViewById(R.id.textquestion);
+        Q1 =(TextView) findViewById(R.id.txtQ1);
+        Q2 =(TextView) findViewById(R.id.txtQ2);
+        Q3 =(TextView) findViewById(R.id.txtQ3);
+        Q4 =(TextView) findViewById(R.id.txtQ4);
+
+
         timeTest = (TextView) findViewById(R.id.timeTest);
         viewAnimator = (ViewAnimator)findViewById(R.id.viewanimator);
 
@@ -53,11 +78,16 @@ public class TestActivity extends AppCompatActivity {
         seleccion=false;
         finalizar=false;
 
-        CardView  cardview = new CardView(this);
-        cardview.setPadding(2,2,2,2);
+        asyncService = AsyncApp42ServiceApi.instance(this);
+        // temporal
+        progressDialog = ProgressDialog.show(this, "", "Searching..");
+        progressDialog.setCancelable(true);
+        asyncService.findDocByDocId(Constants.App42DBName, "diagnosticos", docId, this);
+        // definir primera pregunta del test
+
+
 
         //relativeLayout.addView(cardview);
-        sincronizatedTest();
 
     }
 
@@ -79,4 +109,70 @@ public class TestActivity extends AppCompatActivity {
         }.start();
     }
 
+    @Override
+    public void onDocumentInserted(Storage response) {
+
+    }
+
+    @Override
+    public void onUpdateDocSuccess(Storage response) {
+
+    }
+
+    @Override
+    public void onFindDocSuccess(Storage response) {
+        createAlertDialog("Document SuccessFully Fetched : "+ response.getJsonDocList().get(0).getJsonDoc());
+        docDocument=response.getJsonDocList().get(0).getJsonDoc();
+        try {
+            sincronizatedTest();
+            JSONObject jsonObject = new JSONObject(docDocument);
+            jsonArray = new JSONArray(jsonObject.getString("preguntas"));
+            JSONObject jsonObject1  = jsonArray.getJSONObject(0);
+            textquestion.setText(jsonObject1.getString("descripcion"));
+
+            JSONObject jsonObject2  = new JSONObject(jsonObject1.getString("respuesta1"));
+            Q1.setText(jsonObject2.getString("descripcion"));
+            JSONObject jsonObject3  = new JSONObject(jsonObject1.getString("respuesta2"));
+            Q2.setText(jsonObject3.getString("descripcion"));
+            JSONObject jsonObject4  = new JSONObject(jsonObject1.getString("respuesta3"));
+            Q3.setText(jsonObject4.getString("descripcion"));
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        progressDialog.dismiss();
+    }
+
+    @Override
+    public void onInsertionFailed(App42Exception ex) {
+
+    }
+
+    @Override
+    public void onFindDocFailed(App42Exception ex) {
+        progressDialog.dismiss();
+        createAlertDialog("Exception Occurred : "+ ex.getMessage());
+    }
+
+    @Override
+    public void onUpdateDocFailed(App42Exception ex) {
+
+    }
+    /**
+     * Creates the alert dialog.
+     *
+     * @param msg the msg
+     */
+    public void createAlertDialog(String msg) {
+        AlertDialog.Builder alertbox = new AlertDialog.Builder(TestActivity.this);
+        alertbox.setTitle("Response Message");
+        alertbox.setMessage(msg);
+        alertbox.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            // do something when the button is clicked
+            public void onClick(DialogInterface arg0, int arg1)
+            {
+            }
+        });
+        alertbox.show();
+    }
 }
